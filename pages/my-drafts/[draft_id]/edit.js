153a3337d7df5a8622e0a47883cdc5206c507b32
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-01-21 21:29:50 +0100

    improve draft editor design in /edit and add loading spinner

--- pages/my-drafts/[draft_id]/edit.js
+++ pages/my-drafts/[draft_id]/edit.js
@@ -8,6 +8,7 @@ export default function EditDraft() {
     const router = useRouter()
     const { draft_id } = router.query
 
+    const [ loading, setLoading ] = useState(false)
     const [ subjectLine, setSubjectLine ] = useState('')
     const [ emailText, setEmailText ] = useState('')
     const [ drafts, setDrafts ] = useState([])
@@ -81,7 +82,10 @@ export default function EditDraft() {
 
     function addNewDraft() {
       if (emailText.length >= 2) {
+        setLoading(true)
         editDraft()
+        setTimeout(() => setLoading(false), 2000)
+        setTimeout(() => router.push("/my-drafts"), 2000)
       }
     }
 
@@ -129,14 +133,31 @@ export default function EditDraft() {
               </div>
               </div>
               
-              <textarea 
-                value={emailText} 
-                placeholder='What are you thinking about today?' 
-                onChange={handleEmailText}
-                className=" px-3 py-2 text-xl border-2 border-blue-600 outline-none rounded-lg scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-blue-50" rows={22} cols={24}>
-                
-              </textarea>
-              <button type="button" onClick={addNewDraft} className="bg-blue-600 text-gray-50 border-4 transition duration-600 hover:scale-110 active:scale-100 border-blue-600 text-xl font-bold rounded-lg px-3 py-2 mt-4">Edit draft</button>
+              <div className="flex flex-col border-4 border-blue-600 rounded-lg">
+                <textarea 
+                  value={emailText} 
+                  placeholder='What are you thinking about today?' 
+                  onChange={handleEmailText} 
+                  className="text-xl font-semibold text-blue-900 rounded-lg px-3 pt-2 outline-none scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-blue-50" rows={20} cols={24}>
+                </textarea>
+                <div className="bg-blue-50 border-t-2 flex flex-row justify-start border-blue-100 px-2 py-4 rounded-bl-lg rounded-br-lg">
+                  {
+                    loading
+                    ?
+                    <div className="flex flex-row items-center border-2 border-blue-700 px-3 py-2 bg-blue-700 rounded-lg">
+                        <svg role="status" className="w-8 h-8 text-gray-50 animate-spin fill-blue-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                        </svg>
+                        <p className="text-xl text-gray-50 font-bold ml-2">Saving...</p>
+                    </div> 
+                    :
+                    <button type="button" onClick={addNewDraft} className="bg-blue-600 text-gray-50 border-4 transition duration-600 hover:bg-blue-700 hover:border-blue-700 border-blue-600 text-xl font-bold rounded-lg px-3 py-2">
+                      Edit draft
+                    </button>
+                  }
+                </div>
+              </div>
             </div>
         </div>
     )
